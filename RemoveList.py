cdd70ec2da5f0559fb88f265c3604b6097ced430
uttom-kumar <uttom00781@gmail.com>
2025-07-02 04:38:37 -0700

    Remove List

---
+++ RemoveList.py
@@ -0,0 +1,18 @@
+# remove() = the remove() method removes the specified them.
+
+thisList = ['apple', 'banana', 'mango', 20, 50]
+thisList.remove(20)
+thisList.remove("apple")
+print(thisList)
+
+
+# pop () = The pop() method removes specified index
+thisList2 = ['apple', 'banana', 'mango', 20, 50]
+thisList2.pop(0) #remove specified index items
+thisList2.pop() #remove the last items
+print(thisList2)
+
+#clear () = the clear() method removes all items
+thisList3 = ['apple', 'banana', 'mango', 20, 50]
+thisList3.clear()
+print(thisList3)
