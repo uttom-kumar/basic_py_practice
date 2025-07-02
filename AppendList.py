1bb3bd4537062d959b255ede2e11c06e5bd30794
uttom-kumar <uttom00781@gmail.com>
2025-07-02 02:11:00 -0700

    appendlist add

---
+++ AppendList.py
@@ -0,0 +1,5 @@
+Data = ["akash", "apple", "banana", "mango"]
+Data.append("orange")
+# Data.append("orange", "arif", "sagor", "niranjon")
+# list.append() takes exactly one argument
+print(Data)
---
+++ List.py
@@ -0,0 +1,20 @@
+Data = [
+    {
+        "id": 1,
+        "name": "uttom kumar",
+        "department" : "CSE"
+    },
+    {
+        "id": 2,
+        "name": "Niranjon Roy",
+        "department" : "CSE"
+    },
+    {
+        "id": 3,
+        "name": "Akash kumar",
+        "department" : "EEE"
+    }
+]
+
+
+print(Data[1]["name"])
