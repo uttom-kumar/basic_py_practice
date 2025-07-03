f19125a09cacf20871aeae52f9ceba75b5c881a1
uttom-kumar <uttom00781@gmail.com>
2025-07-03 14:10:08 +0600

    loop-list

--- LoopList.py
+++ LoopList.py
@@ -3,3 +3,9 @@ StudentList = ['arif', 'arifa', 'sagor', 'uttom', 'niranjon', 'tarek']
 
 for i in StudentList :
     print(i)
+
+
+# use the range and len function
+fruitList = ['mango', 'orange', 'banana']
+for item in range(len(fruitList)) :
+    print(item)
